2241b60a23a3d2e6a4e9d3a1b57c65ab6247ccc1
Seb <free.sebastian@gmail.com>
2019-09-05 18:47:13 -0400

    safety first

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import './App.css';
 //importing axios from the node modules (in order to make API call)
 import axios from 'axios';
 
+// added this
+
 class App extends Component {
 
  constructor(){
    console.log("Halllo from Constructor");
    super();
    this.state = {
-     jokeArray: [],
+     roundhouse: [],
    }
  }
+
  componentDidMount(){
    console.log('Hello from COMPONENT DID MOUNT');
-   document.addEventListener('mousedown', this.handleClickOutside);
-
    
  }
  componentDidUpdate(){
@@ -26,6 +27,21 @@ class App extends Component {
    console.log('hello from COMPONENT WILL UNMOUNT')
  }
 
+// getManyJokes = (e) => {
+//   axios({
+//     method: 'get',
+//     url: 'http://api.icndb.com/jokes/random?escape=javascript/10',
+//     responseType: 'json'
+//   }).then((res) => {
+//     console.log(res);
+//     //this changes the initial state to the value that we get back from our API call - in this case an array of art objects, and the title of the first art object
+//     this.setState({
+//       roundhouse:res.data.value,
+//       roundhouseName: res.data.value.joke[0]
+//     });
+//   })
+//   e.stopPropagation()
+// }
 
 getJoke = (e) => {
   axios({
@@ -38,7 +54,6 @@ getJoke = (e) => {
     this.setState({
       jokeArray: res.data.value,
       jokeTime: res.data.value.joke
-     //  jokeName: res.dajokeObjects[0].title
     });
   })
   e.stopPropagation()
@@ -57,15 +72,23 @@ handleBodyClick = () => {
  render(){
    console.log('Hello from the RENDER method');
  return (
+
+  
    <div className="App" onClick={this.handleBodyClick}>
      <h1>Chuckin'Norris</h1>
      <p>test test</p>
      <p>{this.state.jokeTime}</p>
      <button onClick={this.getJoke}></button>
 
-     <p>change test</p>
-     
+     <p>{this.state.roundhouseName}</p>
+
+     {/* <button onClick={this.getManyJokes}></button> */}
+
+
    </div>
+
+
+   
   );
  }
 }
